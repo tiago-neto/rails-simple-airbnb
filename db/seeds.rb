f54819c88a0ef132a8aa2a9530987e3b5de4b202
Tiago Neto <tiagocastroneto@yahoo.com>
2023-11-15 16:10:38 +0000

    challenge progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating restaurants..."
+flat1 = {name: "Matosinhos view", address:"Matosinhos", description: "Flat with a sea view", price_per_night:"80", number_of_guests:"4"}
+falt2 = {name: "Porto view", address:"Porto", description: "Flat with a river view", price_per_night:"150", number_of_guests:"4"}
+falt3 = {name: "Gaia wine", address:"Gaia", description: "Flat with near wine cellars", price_per_night:"100", number_of_guests:"4"}
+falt4 = {name: "Beach side", address:"Leça da Palmeira", description: "Flat near the beach", price_per_night:"150", number_of_guests:"4"}
+
+
+[flat1, falt2, falt3, falt4].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+puts "Finished!"
